05912ad59ae4f7eb8000a07633514f4db8f6d9a1
kod3a <5od3a.yes@protonmail.com>
2022-04-26 07:06:07 -0400

    Update 1.1

--- codice.py
+++ codice.py
@@ -10,7 +10,7 @@ crypt_list = ['RSA','ELG','DSA','ECDH','ECDSA','EDDSA','IDEA','3DES','CAST5','BL
 
 if args.crypt != None and args.user != None and args.app != None and args.len != None and args.out != None:
     if crypt not in crypt_list:
-        sys.exit(color.RED+"invalid algorithm")
+        sys.exit(color.RED+"[-] invalid algorithm")
     try:
         int(args.len)
     except (ValueError) as e:
@@ -34,7 +34,7 @@ if args.crypt != None and args.user != None and args.app != None and args.len !=
         store()
         crypt_f()
         
-    print(color.GREEN+"* Password saved to "+os.path.splitext(outf)[0]+'.gpg')
+    print(color.GREEN+"[*] Password saved to "+os.path.splitext(outf)[0]+'.gpg')
 
 else:
     parser.error("A required argument is missing")
--- mods/functions.py
+++ mods/functions.py
@@ -7,16 +7,17 @@ from mods.colors import *
 from mods.args import *
         
 def decrypt_f():
-    print(color.BLUE+"+ Decrypting "+outf+"..."+color.END)
+    print(color.BLUE+"Decrypting "+outf+"..."+color.END)
     try:
+        subprocess.call(['gpgconf', '--kill', 'gpg-agent'])
         subprocess.check_output(['gpg', outf], stderr=subprocess.STDOUT)
-        print(color.GREEN+"+ Decryption successful"+color.END)
+        print(color.GREEN+"[+] Decryption successful"+color.END)
     except subprocess.CalledProcessError:
-        sys.exit(color.RED+"\n! Exiting..."+color.END)
+        sys.exit(color.RED+"\n[-] Decryption failed..."+color.END)
     
 def gen_pass():
     global password
-    print(color.BLUE+"+ Generating random password of size "+plen+"..."+color.END)
+    print(color.BLUE+"Generating random password of size "+plen+"..."+color.END)
     password = ''
     source = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
     pass_list = list(source)
@@ -24,23 +25,23 @@ def gen_pass():
     
     for i in range(int(plen)):
         password += random.choice(pass_list)
-    print(color.GREEN+"+ Password generated: "+password+color.END)
+    print(color.GREEN+"[+] Password generated: "+password+color.END)
 
 def store():
-    print(color.BLUE+"+ Storing data in "+os.path.splitext(outf)[0]+"..."+color.END)
+    print(color.BLUE+"Storing data in "+os.path.splitext(outf)[0]+"..."+color.END)
     new_line = "\t-"+(str(app)).upper()+':\n'+str(user)+':'+str(password)
     with open(os.path.splitext(outf)[0], 'a') as o:
-        o.write('\n'+new_line)
-        print(color.GREEN+"+ Password has been stored"+color.END)
+        o.write(new_line+'\n')
+        print(color.GREEN+"[+] Password has been stored"+color.END)
 
 def crypt_f():
-    print(color.BLUE+"+ Encrypting "+os.path.splitext(outf)[0]+"..."+color.END)
+    print(color.BLUE+"Encrypting "+os.path.splitext(outf)[0]+"..."+color.END)
     try:
         subprocess.check_output(['gpg', '-c', '-crypt-algo='+str(crypt), os.path.splitext(outf)[0]],
     stderr=subprocess.STDOUT)
-        print(color.GREEN+"+ Encryption successful"+color.END)
+        print(color.GREEN+"[+] Encryption successful"+color.END)
     except subprocess.CalledProcessError:
-        sys.exit(color.RED+"\n! Exiting..."+color.END)
+        sys.exit(color.RED+"\n[-] Encryption failed..."+color.END)
     try:
         os.remove(os.path.splitext(outf)[0])
     except:
