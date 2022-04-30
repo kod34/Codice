54f798f4948eb8f322ec1ce59ffa7215c014e92c
kod34 <5od3a.yes@protonmail.com>
2022-04-30 07:40:19 -0400

    Update 1.1.1

--- codice.py
+++ codice.py
@@ -10,7 +10,7 @@ crypt_list = ['RSA','ELG','DSA','ECDH','ECDSA','EDDSA','IDEA','3DES','CAST5','BL
 
 if args.crypt != None and args.user != None and args.app != None and args.len != None and args.out != None:
     if crypt not in crypt_list:
-        sys.exit(color.RED+"[-] invalid algorithm")
+        sys.exit(color.RED+"[-] Invalid algorithm")
     try:
         int(args.len)
     except (ValueError) as e:
@@ -25,6 +25,8 @@ if args.crypt != None and args.user != None and args.app != None and args.len !=
     
         
     if '.gpg' == os.path.splitext(outf)[1]:
+        if not os.path.exists(outf):
+            sys.exit(color.RED+"[-] Invalid path")
         decrypt_f()
         gen_pass()
         store()
